8616da52eac8204b10c9ce2965af117e724bc363
LyonAlpha <shivan.thaker@gmail.com>
2022-06-04 14:15:33 -0400

    Easier To Use Cpprun When Combined With Cppnew or Cppmake

--- Python Files/cpprun.py
+++ Python Files/cpprun.py
@@ -1,6 +1,7 @@
 import os
 try:
     os.system('gcc -o main.exe  main.cpp -lstdc++')
+    os.system('cd src')
     os.system('.\main.exe')
 except Exception as e:
     os.system('echo The Exception Is The Following: ')
