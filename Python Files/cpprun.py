a6ff74bde75bfa03850b70ee2ef550e4ec34ed95
LyonAlpha <shivan.thaker@gmail.com>
2022-06-04 14:29:26 -0400

    Tried Something Failed, Fixed Error, Back To Previous Version

--- Python Files/cpprun.py
+++ Python Files/cpprun.py
@@ -1,7 +1,6 @@
 import os
 try:
     os.system('gcc -o main.exe  main.cpp -lstdc++')
-    os.system('cd src')
     os.system('.\main.exe')
 except Exception as e:
     os.system('echo The Exception Is The Following: ')
